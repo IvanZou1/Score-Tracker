9cef81357d48cb530abaed13e4c8fbf21740bda2
Ivan Zou <ivanzou2@gmail.com>
2022-05-12 15:38:29 -0400

    initial commit

---
+++ src/Scores.java
@@ -0,0 +1,105 @@
+import java.io.*;
+import java.util.LinkedList;
+
+public class Scores {
+    private int highScore, score;
+    private final LinkedList<Integer> allScores;
+
+    public Scores() {
+        score = 0;
+        setHighScoreFromFile();
+        allScores = new LinkedList<>();
+    }
+
+    public void setHighScoreFromFile() {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("files/highscore.txt"));
+            String line = reader.readLine();
+            highScore = getHighScoreFromString(line);
+            reader.close();
+        } catch (IOException exception) {
+            // File Does Not Exist
+            highScore = 0;
+        }
+
+    }
+
+    private int getHighScoreFromString(String line) {
+        int high;
+        try {
+            high = Integer.parseInt(line.substring(12));
+        } catch (IndexOutOfBoundsException |
+                NullPointerException |
+                NumberFormatException exception) {
+            high = 0;
+        }
+        return high;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScores(int score) {
+        if (score >= 0) {
+            this.score = score;
+            if (highScore < score) {
+                highScore = score;
+            }
+        } else {
+            this.score = 0;
+        }
+    }
+
+    public void reset() {
+        score = 0;
+    }
+
+    public int getHighScore() {
+        return highScore;
+    }
+
+    public LinkedList<Integer> getAllRecordedScores() {
+        return allScores;
+    }
+
+    public void recordScore() {
+        allScores.add(score);
+    }
+
+    public void writeToFile() {
+        writeScoresToFile();
+        writeHighScoreToFile();
+    }
+
+    private void writeScoresToFile() {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("files/scores.txt", false));
+            int gameNumber = 0;
+            writer.write("Scores:");
+            writer.newLine();
+            for (int score: allScores) {
+                gameNumber++;
+                writer.write("game " + gameNumber + ": " + score);
+                writer.newLine();
+            }
+            writer.flush();
+            writer.close();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    private void writeHighScoreToFile() {
+        try {
+            String stringOfHighScore = "High Score: " + highScore;
+            BufferedWriter writer = new BufferedWriter(
+                    new FileWriter("files/highscore.txt", false));
+            writer.write(stringOfHighScore);
+            writer.flush();
+            writer.close();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
