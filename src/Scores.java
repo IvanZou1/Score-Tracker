9180fa05ae26b88b5378366efede0d4eb2555df6
Ivan Zou <ivanzou2@gmail.com>
2022-05-13 07:17:11 -0400

    added explicit imports

--- src/Scores.java
+++ src/Scores.java
@@ -1,4 +1,9 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import java.util.LinkedList;
 
 public class Scores {
