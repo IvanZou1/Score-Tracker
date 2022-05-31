45c96e984c76acfd9a9d26c8e4ded19fabbd188a
Ivan Zou <ivanzou2@gmail.com>
2022-05-31 13:26:07 -0400

    first attempt at JavaDocs

--- src/Scores.java
+++ src/Scores.java
@@ -3,32 +3,55 @@ import java.io.FileReader;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-
 import java.util.LinkedList;
 
+ /**
+  * A {@link Scores} object tracks the scores and the high score in a game application,
+  * and save them to files.
+  *
+  * @author IvanZou1
+  * @version 1.0
+  * @since 05/12/2022
+ */
 public class Scores {
     private final LinkedList<Integer> allScores;
     private int highScore, score;
 
+
+     /**
+      * Default Scores constructor - Initializes {@link Scores#allScores} to a
+      * new LinkedList, {@link Scores#highScore} to a value from the high score
+      * file, and {@link Scores#score} to 0.
+      */
     public Scores() {
-        score = 0;
-        setHighScoreFromFile();
         allScores = new LinkedList<>();
+        setHighScoreFromFile();
+        score = 0;
     }
 
-    public void setHighScoreFromFile() {
+     /**
+      * Reads highscore.txt and sets the value in the file to {@link Scores#highScore}.
+      * If the file does not exist, {@link Scores#highScore} is set to 0.
+      */
+    private void setHighScoreFromFile() {
         try {
             BufferedReader reader = new BufferedReader(new FileReader("files/highscore.txt"));
             String line = reader.readLine();
             highScore = getHighScoreFromString(line);
             reader.close();
         } catch (IOException exception) {
-            // File Does Not Exist
             highScore = 0;
         }
 
     }
 
+     /**
+      * A helper method that returns an integer value of the high score from a
+      * line in highscore.txt.
+      *
+      * @param line a string containing the high score value
+      * @return the int value in the specified line
+      */
     private int getHighScoreFromString(String line) {
         int high;
         try {
@@ -41,10 +64,21 @@ public class Scores {
         return high;
     }
 
+     /**
+      * Getter method for {@link Scores#score}.
+      *
+      * @return {@link Scores#score}
+      */
     public int getScore() {
         return score;
     }
 
+     /**
+      * Sets {@link Scores#score} to the input, and {@link Scores#highScore}
+      * to the input if the input is greater than {@link Scores#highScore}.
+      *
+      * @param score the current score in the game application
+      */
     public void setScores(int score) {
         if (score >= 0) {
             this.score = score;
@@ -56,27 +90,51 @@ public class Scores {
         }
     }
 
+     /**
+      * Resets {@link Scores#score} to 0.
+      */
     public void reset() {
         score = 0;
     }
 
+     /**
+      * Getter method for {@link Scores#highScore}.
+      *
+      * @return {@link Scores#highScore}
+      */
     public int getHighScore() {
         return highScore;
     }
 
+
+     /**
+      * Getter method for {@link Scores#allScores}.
+      *
+      * @return {@link Scores#allScores}
+      */
     public LinkedList<Integer> getAllRecordedScores() {
         return allScores;
     }
 
+     /**
+      * Adds {@link Scores#score} to {@link Scores#allScores}.
+      */
     public void recordScore() {
         allScores.add(score);
     }
 
+     /**
+      * Writes {@link Scores#allScores} and {@link Scores#highScore} to their
+      * respective files.
+      */
     public void writeToFile() {
         writeScoresToFile();
         writeHighScoreToFile();
     }
 
+     /**
+      * Writes {@link Scores#allScores} to scores.txt.
+      */
     private void writeScoresToFile() {
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter("files/scores.txt", false));
@@ -95,6 +153,9 @@ public class Scores {
         }
     }
 
+     /**
+      * Writes {@link Scores#highScore} to highscore.txt.
+      */
     private void writeHighScoreToFile() {
         try {
             String stringOfHighScore = "High Score: " + highScore;
